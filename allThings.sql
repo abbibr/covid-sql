bd06e1e9b5d085d66409f9baf25525b833ce242b
abbibr <ibrohim_abbosov@mail.ru>
2024-11-26 15:25:00 +0500

    added new clauses

--- allThings.sql
+++ allThings.sql
@@ -1,21 +1,42 @@
-/* Select statement */
+/* Select clause */
 SELECT id, country, date, vaccine, population 
 FROM covid_vaccinations_dataset
 
 
-/* Limit statement */
+/* Limit clause */
 SELECT *
 FROM covid_vaccinations_dataset
 LIMIT 50
 
 
-/* Order By statement*/
+/* Order By clause*/
 SELECT * 
 FROM covid_vaccinations_dataset
 ORDER BY date ASC
 LIMIT 10
 
 
-/* Distinct statement */
+/* Distinct clause */
 SELECT DISTINCT(date)
 FROM covid_vaccinations_dataset
+
+
+/* Where clause */
+SELECT *
+FROM covid_vaccinations_dataset
+WHERE id BETWEEN 300 AND 500
+
+
+/* Count clause */
+SELECT Count(id)
+FROM covid_vaccinations_dataset
+
+
+/* Group By clause */
+SELECT *, COUNT(date)
+FROM covid_vaccinations_dataset
+GROUP BY date
+ORDER BY date ASC
+
+
+/* Max, Min, Avg clauses */
